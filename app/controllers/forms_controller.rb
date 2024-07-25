10b074a43c0695b3f5a26f3d2958454c74744f67
apatel-ai <arpan.p.se@gmail.com>
2024-07-24 20:28:34 -0500

    created forms page,refactored routes for forms and users.

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,4 +1,5 @@
 class FormsController < ApplicationController
+  skip_before_action :authenticate_user!
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
-  get 'forms/index'
-
-
-  get '/forms', to: 'forms#index'
-
+  get "/forms", to: "forms#index"
 
   resources :reminders
   resources :documents
   resources :dependents
   root "dashboard#greetings"
 
-
-
   devise_for :users
   get "/:username" => "users#show", as: :user
-
-  # get "/your_first_screen" => "pages#first"
 
 end
