050166359195b83eb5a9b8cbc8b390d60e766768
apatel-ai <arpan.p.se@gmail.com>
2024-07-26 19:20:00 -0500

    created signed_in and signed_out dashboard view templates

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class DashboardController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:greetings, :home]
+
+  def home
+    if user_signed_in?
+      redirect_to dashboard_path
+    else
+      redirect_to landing_path
+    end
+  end
+
   def greetings
     @message = "Welcome to Pathway"
   end
