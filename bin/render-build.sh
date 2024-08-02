6baa9027de3d397fc4991aee9d60b2217cf6ac8b
apatel-ai <arpan.p.se@gmail.com>
2024-08-02 16:03:04 -0500

    testing for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,11 +5,6 @@ set -o errexit
 bundle install
 
 # For Ruby on Rails apps uncomment these lines to precompile assets and migrate your database.
-bundle exec rake assets:precompile
+bundle exec rake assets:precompile --trace
 bundle exec rake assets:clean
 bundle exec rake db:migrate
-
-
-# Debugging output
-echo "Listing assets in public/assets:"
-ls -l public/assets
