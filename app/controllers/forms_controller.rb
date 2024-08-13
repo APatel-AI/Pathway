3f4f490404fee13f636f3112127f8d8ee06b9e68
apatel-ai <arpan.p.se@gmail.com>
2024-08-13 18:51:19 -0500

    added filter feature for uscis forms

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -4,6 +4,8 @@ class FormsController < ApplicationController
   def index
     forms = UscisScraper.scrape_forms
 
+
+    # Filter forms based on the search query
     if params[:query].present?
       query = params[:query].downcase
       forms = forms.select do |form|
@@ -11,9 +13,46 @@ class FormsController < ApplicationController
       end
     end
 
+
+    @categories = [
+      "Forms",
+      "Adoptions-Based Forms",
+      "Citizenship and Naturalization-Based Forms",
+      "Employment-based Forms",
+      "Family-based Forms",
+      "Green Card-based Forms",
+      "Humanitarian Benefits-based Forms",
+
+    ]
+
+    if params[:category].present?
+      forms = forms.select { |form| categorize_form(form[:name]) == params[:category] }
+    end
+
     @forms = WillPaginate::Collection.create(params[:page] || 1, 10, forms.length) do |pager|
       start = pager.offset
       pager.replace forms[start, pager.per_page]
+    end
+  end
+
+  private
+
+  def categorize_form(form_name)
+    case form_name.downcase
+    when /adoption/
+      "Adoptions-Based Forms"
+    when /citizenship|naturalization/
+      "Citizenship and Naturalization-Based Forms"
+    when /employment/
+      "Employment-based Forms"
+    when /family/
+      "Family-based Forms"
+    when /green card|permanent resident/
+      "Green Card-based Forms"
+    when /humanitarian|benefit/
+      "Humanitarian Benefits-based Forms"
+    else
+      "Forms"
     end
   end
 end
