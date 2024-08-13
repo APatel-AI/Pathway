29ab043271dcf96742715e03b3012b86d32f4478
apatel-ai <arpan.p.se@gmail.com>
2024-08-13 09:05:51 -0500

    added breadcrumbs to dependents controller and fixed update profile error

--- app/controllers/dependents_controller.rb
+++ app/controllers/dependents_controller.rb
@@ -1,25 +1,34 @@
 class DependentsController < ApplicationController
   before_action :set_dependent, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_creator, only: %i[show edit update destroy]
+  before_action :set_breadcrumbs, only: %i[index show new edit]
 
   # GET /dependents or /dependents.json
   def index
     @dependents = current_user.dependents
+    add_breadcrumb "Dependents", dependents_path
   end
 
   # GET /dependents/1 or /dependents/1.json
   def show
     @dependent = Dependent.find(params[:id])
     @documents = @dependent.documents
+    add_breadcrumb "Dependents", dependents_path
+    add_breadcrumb @dependent.first_name, dependent_path(@dependent)
   end
 
   # GET /dependents/new
   def new
     @dependent = Dependent.new
+    add_breadcrumb "Dependents", dependents_path
+    add_breadcrumb "New Dependent", new_dependent_path
   end
 
   # GET /dependents/1/edit
   def edit
+    add_breadcrumb "Dependents", dependents_path
+    add_breadcrumb @dependent.first_name, dependent_path(@dependent)
+    add_breadcrumb "Edit Dependent", edit_dependent_path(@dependent)
   end
 
   # POST /dependents or /dependents.json
@@ -62,20 +71,25 @@ class DependentsController < ApplicationController
 
   private
 
-    def set_dependent
-      @dependent = Dependent.find(params[:id])
+  def set_dependent
+    @dependent = Dependent.find(params[:id])
+  end
+
+  def ensure_current_user_is_creator
+    if @dependent.user_id != current_user.id
+      redirect_back fallback_location: root_url, alert: "You're not authorized for that."
     end
+  end
 
-    def ensure_current_user_is_creator
-      if @dependent.user_id !=  current_user.id
-        redirect_back fallback_location: root_url, alert: "You're not authorized for that."
-      end
+  def set_breadcrumbs
+    @breadcrumbs = []
+  end
 
-    end
+  def add_breadcrumb(content, href = nil)
+    @breadcrumbs << { content: content, href: href }
+  end
 
-
-    # Only allow a list of trusted parameters through.
-    def dependent_params
-      params.require(:dependent).permit(:user_id, :image, :address, :relationship, :first_name, :last_name, :date_of_birth, :immigration_status, :country_of_citizenship, :contact_info)
-    end
+  def dependent_params
+    params.require(:dependent).permit(:user_id, :image, :address, :relationship, :first_name, :last_name, :date_of_birth, :immigration_status, :country_of_citizenship, :contact_info)
+  end
 end
