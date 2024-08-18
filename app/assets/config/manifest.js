70e893438b658162c8c5240cd326417c8e2509ea
APatel-AI <arpan.p.se@gmail.com>
2024-08-18 18:21:23 -0400

    added features section to landing page

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link access_forms.mov
+//= link create_profiles.mov
+//= link upload_docs.mov
+//= link set_reminders.mov
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  rails assets:precompile
+
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
